81bdd63ecbfd95ab5111ce23cbde5fba4f25247e
Ahmad Saeed <ahmad.saeed.goda@gmail.com>
2019-06-28 21:24:57 +0200

    Code Standardizing Against PSR1 & PSR2
    Use PSR1 and PSR2 along with PSR12 as a code standard & style to be followed in future commits.

--- app/Repositories/RepositoryInterface.php
+++ app/Repositories/RepositoryInterface.php
@@ -6,7 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Pagination\LengthAwarePaginator;
 
-
 /**
  * Contract to access database using an ORM
  */
